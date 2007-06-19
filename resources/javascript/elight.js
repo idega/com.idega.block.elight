de3bc4f4b8ed223f33ad0ea384ad5029e07369fc
Vytautas Civilis <civilis@idega.com>
2007-06-19 06:56:07 +0000

    absolute positioning in respect to search button (image) added

--- resources/javascript/elight.js
+++ resources/javascript/elight.js
@@ -3,21 +3,29 @@ if(ElightResult == null) var ElightResult = function() {};
 
 /* ------ elight ------ */
 
+Elight.SEARCH_BUTTON_ID 			= 'elightSearchButton';
+Elight.INPUT_ID 					= 'elightInput';
+Elight.OUTPUT_ID 					= 'elightOutput';
+Elight.RESULTS_ID 					= 'elightResults';
+Elight.SEARCH_INPUT_ID 				= 'elightSearchInput';
+Elight.SLIDED_OUT					= 1;
+Elight.SLIDED_IN					= 2;
+
 Elight.getSearchResults = function(data) {
-	
-	var container = $('elightResults');
+
+	var container = $(Elight.RESULTS_ID);
 	
 	removeChildren(container);
 	
 	if(!data || data == null || data.length == 0) {
 		Elight.addMessage("Sorry. No data was retrieved.", container);
-		$('elightSearchButton').src = elight_search_uri;
+		$(Elight.SEARCH_BUTTON_ID).src = elight_search_uri;
 		return;
 	}
 	
 	if(data.length == 1 && data[0].length == 1  && data[0][0].message != null) {
 		Elight.addMessage(data[0][0].message, container);
-		$('elightSearchButton').src = elight_search_uri;
+		$(Elight.SEARCH_BUTTON_ID).src = elight_search_uri;
 		return;
 	}
 	
@@ -31,7 +39,7 @@ Elight.getSearchResults = function(data) {
 		Elight.addResultEntry(data[index], container);
 	}
 	
-	$('elightSearchButton').src = elight_search_uri;
+	$(Elight.SEARCH_BUTTON_ID).src = elight_search_uri;
 }
 
 
@@ -160,31 +168,29 @@ ElightResult.prototype.getResultDOM = function() {
 
 window.addEvent('domready', function() {
 	
-	var elight_SLIDED_OUT 	=	1;
-	var elight_SLIDED_IN	=	2;
-	
-	var elight_horizontal_slide = new Fx.Slide('elightInputText', {mode: 'horizontal', duration: 300});
-	elight_horizontal_slide.hide();
-	elight_horizontal_slide.state = elight_SLIDED_OUT;
-//	elight_horizontal_slide.state = elight_SLIDED_IN;
 
-	$('elightSearchButton').addEvent('click', function(e) {
+	
+	var elight_horizontal_slide = new Fx.Slide('elightInputText', {mode: 'horizontal', duration: 300}).hide();
+	elight_horizontal_slide.state = Elight.SLIDED_OUT;
+//	elight_horizontal_slide.state = Elight.SLIDED_IN;
+
+	$(Elight.SEARCH_BUTTON_ID).addEvent('click', function(e) {
 		e = new Event(e);
 		
-		$('elightSearchInput').focus();
+		$(Elight.SEARCH_INPUT_ID).focus();
 		
-		if(elight_horizontal_slide.state == elight_SLIDED_IN) {
+		if(elight_horizontal_slide.state == Elight.SLIDED_IN) {
 			
 			elight_horizontal_slide.state = 1;
 			elight_horizontal_slide.slideOut();
 			
 		} else {
 
-			elight_horizontal_slide.state = elight_SLIDED_IN;
+			elight_horizontal_slide.state = Elight.SLIDED_IN;
 			elight_horizontal_slide.slideIn();
 		}
 		
-		if(elight_results_slide.state == elight_SLIDED_IN)
+		if(elight_results_slide.state == Elight.SLIDED_IN)
 			elight_results_slide.slideOut();
 
 		e.stop();
@@ -192,30 +198,46 @@ window.addEvent('domready', function() {
 	});
 
 	
-	var elight_results_slide = new Fx.Slide('elightResults', {mode: 'vertical'});
-	elight_results_slide.hide();
-	elight_results_slide.state = elight_SLIDED_OUT;
-//	elight_results_slide.state = elight_SLIDED_IN;
+	var elight_results_slide = new Fx.Slide(Elight.RESULTS_ID, {mode: 'vertical'}).hide();
+	elight_results_slide.state = Elight.SLIDED_OUT;
+//	elight_results_slide.state = Elight.SLIDED_IN;
 
-	$('elightSearchInput').addEvent('keypress', function(e) {
+	$(Elight.SEARCH_INPUT_ID).addEvent('keypress', function(e) {
 		
 		if(!isEnterEvent(e))
 			return;
 			
-		$('elightSearchButton').src = elight_working_uri;
+		$(Elight.SEARCH_BUTTON_ID).src = elight_working_uri;
 		
-		ElightSearchResults.search($('elightSearchInput').value, elight_pu_param, Elight.getSearchResults);
+		ElightSearchResults.search($(Elight.SEARCH_INPUT_ID).value, elight_pu_param, Elight.getSearchResults);
 			
 		e = new Event(e);
 		
-		elight_results_slide.state = elight_SLIDED_IN;
+		elight_results_slide.state = Elight.SLIDED_IN;
 		elight_results_slide.slideIn();
 
 		e.stop();
 	});
 	
-	var scrolly = new Scroller('elightResults', {area: 80, velocity: 0.7});
+	var elight_scrolly = new Scroller(Elight.RESULTS_ID, {area: 80, velocity: 0.7});
 	
-	$('elightResults').addEvent('mouseover', scrolly.start.bind(scrolly));
-	$('elightResults').addEvent('mouseout', scrolly.stop.bind(scrolly));
+	$(Elight.RESULTS_ID).addEvent('mouseover', elight_scrolly.start.bind(elight_scrolly));
+	$(Elight.RESULTS_ID).addEvent('mouseout', elight_scrolly.stop.bind(elight_scrolly));
+	
+//	positioning
+	var elight_top_position = getAbsoluteTop(Elight.SEARCH_BUTTON_ID);
+	var elight_left_position = getAbsoluteLeft(Elight.SEARCH_BUTTON_ID);
+	
+	console.log(elight_top_position);
+	console.log(elight_left_position);
+	
+	var elight_input_div = $(Elight.INPUT_ID).getElementsByTagName('div')[0];
+	var elight_output_div = $(Elight.OUTPUT_ID).getElementsByTagName('div')[0];
+
+	elight_input_div.style.top = (elight_top_position + 8)+"px";
+	elight_input_div.style.left = (elight_left_position + 40)+"px";
+	
+	elight_output_div.style.top = (elight_top_position + 8+25)+"px";
+	elight_output_div.style.left = (elight_left_position + 40)+"px";
+
 });
